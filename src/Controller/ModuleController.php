c14d79e85504476d4f07f093d0824aee72f37802
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-22 13:52:43 +0200

    controllers: created controllers

---
+++ src/Controller/ModuleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ModuleController extends AbstractController
+{
+    #[Route('/module', name: 'app_module')]
+    public function index(): Response
+    {
+        return $this->render('module/index.html.twig', [
+            'controller_name' => 'ModuleController',
+        ]);
+    }
+}
