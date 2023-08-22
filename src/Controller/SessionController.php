e63de4c7a1c412930e9a555e902a74bbd413d06a
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-22 16:58:21 +0200

    SessionCtrl: added $sessions to index method

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
+use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository, FormationRepository $formationRepository): Response
     {
+        $sessions = $sessionRepository->findBy([], ["dateDebut" => "ASC"]);
+
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
         ]);
     }
 }
