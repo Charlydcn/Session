8171794e28381b14f98dc0b877af6a74bf3a3361
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-23 15:43:30 +0200

    StagiaireCtrl: stagiaires list in index method

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StagiaireRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    public function index(StagiaireRepository $stagiaireRepository): Response
     {
+        $stagiaires = $stagiaireRepository->findBy([], ["nom" => "ASC"]);
+
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaires,
         ]);
     }
 }
