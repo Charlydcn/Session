e273e42f4ea55bbe5b9511c8ad5d275083b14333
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-23 15:43:04 +0200

    FormationCtrl: formations list in index method

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
+use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository, SessionRepository $sessionRepository): Response
     {
+        $formations = $formationRepository->findBy([], ["intitule" => "ASC"]);
+
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations,
         ]);
     }
 }
