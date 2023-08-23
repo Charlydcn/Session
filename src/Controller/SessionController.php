23becedb1be7c6a542f36194a8c4a7c41a2c4763
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-23 15:43:18 +0200

    SessionCtrl: sessions list in index method

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -11,12 +11,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(SessionRepository $sessionRepository, FormationRepository $formationRepository): Response
+    public function index(FormationRepository $formationRepository): Response
     {
-        $sessions = $sessionRepository->findBy([], ["dateDebut" => "ASC"]);
+        // $sessions = $sessionRepository->findBy([], ["dateDebut" => "ASC"]);
+        $formations = $formationRepository->findBy([], ["intitule" => "ASC"]);
 
         return $this->render('session/index.html.twig', [
-            'sessions' => $sessions,
+            // 'sessions' => $sessions,
+            'formations' => $formations,
         ]);
     }
+
+    // requête pour avoir la date la plus proche:
+    // SELECT date_debut
+    // FROM session
+    // WHERE date_debut > CURDATE() & formation_id = :id
+    // ORDER BY date_debut ASC
+    // LIMIT 1;
+
+    // peut-être faisable comme ça
+    // $closestSession = $sessionRepository->findBy(["date_debut" => "CURDATE() AND formation_id = $id"], ["date_debut" => "ASC"], 1);        
+
 }
