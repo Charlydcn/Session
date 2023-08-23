fb267bd1a6704d573e3b062409065c6bffbde2b3
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-23 16:57:06 +0200

    moduleCtrl: added missing class imports

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Module;
+use App\Repository\ProgrammeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ModuleController extends AbstractController
 {
