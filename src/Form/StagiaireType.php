758bc0809d401657a628292b7eb397cc99d23cdc
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-24 16:20:09 +0200

    Stagiaire: added data type + other on type input

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -5,25 +5,74 @@ namespace App\Form;
 use App\Entity\Stagiaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
-            ->add('courriel', EmailType::class)
-            ->add('dateNaissance', BirthdayType::class, ["widget" => "single_text"])
-            ->add('ville', TextType::class)
-            ->add('telephone', TextType::class)
-            ->add('sexe', TextType::class)
-            ->add('sessions')
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('courriel', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('dateNaissance', BirthdayType::class, [
+                'required' => false,
+                "widget" => "single_text",
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('telephone', TelType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'constraints' => [
+                    new Length(['min' => 10, 'max' => 20]),
+                ],
+            ])
+
+            ->add('sexe', ChoiceType::class, [
+                'choices'  => [
+                    'Homme' => 'Homme',
+                    'Femme' => 'Femme',
+                    'Autre' => 'Autre',
+                ],
+            ])
+
+            ->add('Valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
