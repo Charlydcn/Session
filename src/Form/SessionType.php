9d0b9845cb428eb7e2af2446b8f1938dff78daf3
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-24 16:20:01 +0200

    SessionType: added data type + other on type input

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -3,20 +3,53 @@
 namespace App\Form;
 
 use App\Entity\Session;
+use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nbPlaces')
-            ->add('dateDebut')
-            ->add('dateFin')
-            ->add('formation')
-            ->add('stagiaires')
+            ->add('nbPlaces', NumberType::class, [
+                'attr' => [
+                    'int' => true,
+                    'class' => 'form-control',
+                ]
+            ])
+
+            ->add('dateDebut', DateType::class, [
+                "widget" => "single_text",
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('dateFin', DateType::class, [
+                "widget" => "single_text",
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('Valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
