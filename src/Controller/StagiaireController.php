207b2c01ef362caf96ffcfe939f3d7d340f02cfa
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-24 16:18:00 +0200

    stagiaireCtrl: created 'new' method

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Stagiaire;
 use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,17 +24,48 @@ class StagiaireController extends AbstractController
     }
 
     #[Route('/stagiaire/new', name: 'new_stagiaire')]
-    public function new(Request $request): Response
+    // #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function new(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $stagiaire = new Stagiaire();
+        // if (!$stagiaire) {
+            $stagiaire = new Stagiaire();
+        // }
+        
+        // On créer le formulaire à partir du formType (stagiaireType)
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
 
-        $newStagiaireForm = $this->createForm(StagiaireType::class, $stagiaire);
+        //  On 'handle' la requête
+        $form->handleRequest($request);
 
+        // Si le formulaire est soumis et valide,
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // on récupère les données
+            $stagiaire = $form->getData();
+            // persist = prepare PDO
+            $entityManager->persist($stagiaire);
+            // flush = execute PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+        
         return $this->render('stagiaire/new.html.twig', [
-            'newStagiaireForm' => $newStagiaireForm,
+            'addStagiaireForm' => $form,
+            // 'edit' => $stagiaire->getId()
         ]);
     }
-    
+
+    #[Route('/stagiaire/{id}/delete', name: 'delete_stagiaire')]
+    public function delete(Stagiaire $stagiaire, EntityManagerInterface $entityManager) 
+    {
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_stagiaire');
+    }
+
+
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
     {   
