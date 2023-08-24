f82dbd8aef2d9f00408801046c5a20835f985be2
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-24 10:57:51 +0200

    sessionCtrl:method show: removed uslss $programmes

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Session;
 use App\Repository\SessionRepository;
 use App\Repository\FormationRepository;
-use App\Repository\ProgrammeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,13 +22,10 @@ class SessionController extends AbstractController
     }
 
     #[Route('/session/{id}', name: 'show_session')]
-    public function show(Session $session, ProgrammeRepository $programmeRepository): Response
-    {   
-        $programmes = $programmeRepository->findBy(["session" => $session->getId()], []);
-
+    public function show(Session $session): Response
+    {
         return $this->render('session/show.html.twig', [
             'session' => $session,
-            'programmes' => $programmes,
         ]);
     }
 
