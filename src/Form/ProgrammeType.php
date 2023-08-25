4318af096b32706e37359442af52e679983ee637
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-25 16:57:36 +0200

    ProgrammeType: added input fields types

--- src/Form/ProgrammeType.php
+++ src/Form/ProgrammeType.php
@@ -5,16 +5,18 @@ namespace App\Form;
 use App\Entity\Programme;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class ProgrammeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nbJours')
-            ->add('session')
-            ->add('module')
+            ->add('nbJours', NumberType::class)
+            ->add('session', EntityType::class)
+            ->add('module', EntityType::class)
         ;
     }
 
