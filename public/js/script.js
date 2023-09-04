946c7c7527ed5d9eeeaabfe6a2eded8207cf0714
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-04 11:33:17 +0200

    script.js: script for responsive nav

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// make <tr> of classes 'row-link' clickable instead of the <a> inside of them
 var rows = document.querySelectorAll('.row-link');
 
 rows.forEach(function (row) {
@@ -5,4 +6,13 @@ rows.forEach(function (row) {
         var url = this.getAttribute('data-href');
         window.location.href = url;
     });
+});
+
+// responsive nav opening/closing
+var openBtn = document.getElementById("openBtn");
+var overlay = document.getElementById("nav-menu");
+openBtn.addEventListener("click", function () {
+    console.log('test')
+	this.classList.toggle("close");
+	overlay.classList.toggle("overlay");
 });
