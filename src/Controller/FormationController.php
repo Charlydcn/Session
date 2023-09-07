6058deb29f811545c12e8a26cd3f0a1802d9538d
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-07 11:30:21 +0200

    Formation: added create/delete functionnalities

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Form\FormationType;
 use App\Repository\SessionRepository;
 use App\Repository\FormationRepository;
 use App\Repository\ProgrammeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +23,48 @@ class FormationController extends AbstractController
         return $this->render('formation/index.html.twig', [
             'formations' => $formations,
         ]);
+    }
+
+    #[Route('/formation/new', name: 'new_formation')]
+    // #[Route('/formation/{id}/edit', name: 'edit_formation')]
+    public function new(Formation $formation = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // if (!$formation) {
+            $formation = new Formation();
+        // }
+        
+        // On créer le formulaire à partir du formType (formationType)
+        $form = $this->createForm(FormationType::class, $formation);
+
+        //  On 'handle' la requête
+        $form->handleRequest($request);
+
+        // Si le formulaire est soumis et valide,
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // on récupère les données
+            $formation = $form->getData();
+            // persist = prepare PDO
+            $entityManager->persist($formation);
+            // flush = execute PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_formation');
+        }
+        
+        return $this->render('formation/new.html.twig', [
+            'addFormationForm' => $form,
+            // 'edit' => $formation->getId()
+        ]);
+    }
+
+    #[Route('/formation/{id}/delete', name: 'delete_formation')]
+    public function delete(Formation $formation, EntityManagerInterface $entityManager) 
+    {
+        $entityManager->remove($formation);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_formation');
     }
 
     #[Route('/formation/{id}', name: 'show_formation')]
---
+++ src/Form/FormationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('intitule', TextType::class, [
+                'label' => 'Intitulé',
+                'constraints' => [
+                    new Length([
+                        'max' => '100',
+                        'maxMessage' => 'L\'Intitulé de la formation ne doit pas dépasser 100 caractères',
+                        ])
+                    ],
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
